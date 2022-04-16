df6a275788361c54aced18d1ffe290102605d4ec
kuhnn <kuhnnat000@gmail.com>
2022-04-16 12:15:17 -0700

    add state machine framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,58 @@
 #include <Arduino.h>
 
+/* Enum responsible for representing the states of flight:
+
+  nav_converge:   calibration of the GPS, IMU, and Barometer
+  pad_idle:       waiting for rocket burn, begin position tracking
+  burn:           begin data logging
+  coast:          dump data if necessary TODO: determine if needed due to space limitations
+  post_apogee:    deploy water from nose cone
+  chute_deployed: dump data if necessary TODO: determine if needed due to space limitations
+  grounded:       end data logging, end position tracking, dump all logged data onto the SD card
+
+*/
+
+enum system_state {
+  nav_converge,
+  pad_idle,
+  burn,
+  coast,
+  post_apogee,
+  chute_deployed,
+  grounded
+} state;
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  state = nav_converge;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  switch (state) {
+
+    case nav_converge:
+      break;
+
+    case pad_idle:
+      break;
+
+    case burn:
+      break;
+    
+    case coast:
+      break;
+
+    case post_apogee:
+      break;
+
+    case chute_deployed:
+      break;
+
+    case grounded:
+      break;
+
+  }
+
 }
