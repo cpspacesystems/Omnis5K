83a3630f93f43a2b5e3704f8c8e28299fa96d362
curtisbucher <cpbucher5@gmail.com>
2022-06-01 21:13:21 -0700

    updated uplink code

--- src/telemetry.cpp
+++ src/telemetry.cpp
@@ -28,17 +28,30 @@ void telemetry_receive() {
     while( TEL.available() > 0 ){
         msg = TEL.readString();
         // Running corresponding functions
-        if(msg.startsWith("funcA"))
+        if(msg.startsWith("ACTION_A_ABC"))
             uplink_funcA(msg);
 
-        else if(msg.startsWith("funcB"))
+        else if(msg.startsWith("ACTION_B_DEF"))
             uplink_funcB(msg);
 
-        else if(msg.startsWith("funcC"))
+        else if(msg.startsWith("ACTION_C_GHI"))
             uplink_funcC(msg);
 
-        else if(msg.startsWith("funcD"))
+        else if(msg.startsWith("ACTION_D_JKL"))
             uplink_funcD(msg);
+
+        else if(msg.startsWith("ACTION_E_MNO"))
+            uplink_funcE(msg);
+
+        else if(msg.startsWith("ACTION_F_PQR"))
+            uplink_funcF(msg);
+
+        else if(msg.startsWith("ACTION_G_STU"))
+            uplink_funcG(msg);
+
+        else if(msg.startsWith("ACTION_H_VWX"))
+            uplink_funcH(msg);
+
         else{
             Serial.println("Command not recognized:");
             Serial.println(msg);
@@ -61,5 +74,21 @@ void uplink_funcC(String input){
 }
 
 void uplink_funcD(String input){
+    Serial.println("Running funcD");
+}
+
+void uplink_funcE(String input){
+    Serial.println("Running funcB");
+}
+
+void uplink_funcF(String input){
+    Serial.println("Running funcC");
+}
+
+void uplink_funcG(String input){
+    Serial.println("Running funcD");
+}
+
+void uplink_funcH(String input){
     Serial.println("Running funcD");
 }
